7bfc23ba2ade0507a7724e870f57e0e4c142b4d1
rahulh07 <iamhedau07@gmail.com>
2022-08-13 03:10:57 +0530

    commited 2nd time

--- src/main/java/demoGit/Classgithub.java
+++ src/main/java/demoGit/Classgithub.java
@@ -1,5 +1,12 @@
 package demoGit;
 
-public class Classgithub {
+public class Classgithub 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("github");
+		System.out.println("hello github");
+		
+	}
 
 }
